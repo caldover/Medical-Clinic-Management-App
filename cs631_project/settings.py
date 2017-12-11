e0696aed5e510b21236e240c9c03ea1449408c1c
Christian Aldover <cka6@njit.edu>
2017-12-10 21:50:06 -0500

    Updating settings to connect to NJIT mysql db

--- cs631_project/settings.py
+++ cs631_project/settings.py
@@ -80,7 +80,8 @@ WSGI_APPLICATION = 'cs631_project.wsgi.application'
 
 # default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 # default_dburl = 'mysql://root:fenderp33@localhost:3306/test_1'
-default_dburl = 'mysql://mc677:pXRwRXIN@sql.njit.edu:3306/mc677'
+# default_dburl = 'mysql://mc677:pXRwRXIN@sql.njit.edu:3306/mc677'
+default_dburl = 'mysql://mc677:pXRwRXIN@128.235.208.216:3306/mc677'
 
 # DATABASES = {
 #     # 'default': {
