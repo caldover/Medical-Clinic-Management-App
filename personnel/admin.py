9e7139804ea75a53b224a04bd2097fe54e7abe16
Christian Aldover <cka6@njit.edu>
2017-12-13 17:40:54 -0500

    reverting back to before shift migration to recreate table3

--- personnel/admin.py
+++ personnel/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Personnel, Physician, Surgeon, Nurse, Shift
+from .models import Personnel, Physician, Surgeon, Nurse, Shift, Schedule
 
 # Register your models here.
 admin.site.register(Personnel)
@@ -7,3 +7,4 @@ admin.site.register(Physician)
 admin.site.register(Surgeon)
 admin.site.register(Nurse)
 admin.site.register(Shift)
+admin.site.register(Schedule)
