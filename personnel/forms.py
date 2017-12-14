ed51cd48914c820015932142342c0787346cc227
Christian Aldover <cka6@njit.edu>
2017-12-14 02:18:20 -0500

    Implementing appointment scheduling15

--- personnel/forms.py
+++ personnel/forms.py
@@ -72,10 +72,5 @@ class ShiftForm(forms.Form):
 
 class PhysicianGetShiftForm(forms.Form):
     employee_no = forms.ModelChoiceField(label='Physician Name', queryset=Physician.objects.all())
-    #date = forms.ModelChoiceField(label='Available Dates', queryset=Shift.objects.all())
-
-    dates = Shift.objects.get(employee_no_id=employee_no.id)
-
-    choices = [(i, i) for i in dates]
-    date = forms.ChoiceField(label='Available Hours', choices=choices)
+    date = forms.ModelChoiceField(label='Available Dates', queryset=Shift.objects.all())
 
