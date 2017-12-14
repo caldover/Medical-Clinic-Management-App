8cba5d3e6a5d649b6e016c97b7afd99a01bd6129
Christian Aldover <cka6@njit.edu>
2017-12-13 20:28:46 -0500

    Implementing availability for physicians7

--- personnel/urls.py
+++ personnel/urls.py
@@ -46,7 +46,7 @@ urlpatterns = [
     url(r'availability/^(?P<employee_no_id>[0-9]+)/^(?P<date>\d{4}-\d{2}-\d{2})/$', views.AvailView.as_view(), name='physician_avail2'),
 
     # /personnel/physicians/availability/employee_no/date/
-    url(r'physicians/availability/^(?P<employee_no_id>[0-9]+)/^(?P<date>\d{4}-\d{2}-\d{2})/$', views.AvailView.as_view(), name='physician_avail'),
+    url(r'physicians/availability/^(?P<employee_no_id>\d+)/(?P<date>\d{4}-\d{2}-\d{2})/$', views.AvailView.as_view(), name='physician_avail'),
     #url(r'physicians/availability/^(?P<employee_no_id>[0-9]+)/^(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})/$', views.AvailView.as_view(), name='physician_avail'),
 
     # /personnel/surgeons/add/
