d87041cfd40bcd46d6914765bbcfa003bb2fd339
Christian <cka6@njit.edu>
2017-12-16 19:46:30 -0500

    creating timeblock model

--- personnel/views.py
+++ personnel/views.py
@@ -333,7 +333,7 @@ def get_physician_shift_info(request):
         if form.is_valid():
             employee_no = form.cleaned_data['employee_no']
             values.current_physician = employee_no.pk
-
+            form = PhysicianGetDateForm(request.POST, request=values.current_physician)
             # redirect to a new URL:
             return HttpResponseRedirect(reverse('personnel:physician_dates', args=[values.current_physician]))
         else:
