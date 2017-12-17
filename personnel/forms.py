0d0ac59db199cf0e7cf68e38b1c6d4c5dd444dc0
Christian <cka6@njit.edu>
2017-12-16 20:22:31 -0500

    creating timeblock model

--- personnel/forms.py
+++ personnel/forms.py
@@ -82,11 +82,12 @@ class PhysicianGetDateForm(forms.Form):
 
     date = forms.ModelChoiceField()
 
-    def __init__(self, *args,**kwargs):
+    def __init__(self, *args, **kwargs):
         request = kwargs.pop('request',None)
         if not 'queryset' in kwargs:
             kwargs['queryset'] = Shift.objects.filter(employee_no_id=values.current_physician)
-        super(PhysicianGetDateForm,self).__init__(*args,**kwargs)
+        return super(PhysicianGetDateForm,self).__init__(*args,**kwargs)
+        #super(PhysicianGetDateForm,self).__init__(*args,**kwargs)
             #self.fields['date'].choices = [x.date for x in Shift.objects.filter(employee_no_id=values.current_physician)]
         # self.fields['date'].choices = zip(
         #     [Shift.objects.filter(employee_no_id=values.current_physician)], [Shift.objects.filter(employee_no_id=values.current_physician)]
